8f7d043e5980745a3743c284251e46d6710577e1
Raphael Awoyinfa <RaphicoGit@gmail.com>
2024-09-05 23:28:05 +0100

    Time: 1053 ms (64.85%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,17 @@
 -- Write your PostgreSQL query statement below
-SELECT 
-    contest_id, 
-    ROUND(COUNT(user_id)::NUMERIC / (SELECT COUNT(*) FROM Users) * 100, 2) AS percentage
-    FROM Register
-    GROUP BY contest_id
+-- SELECT 
+--     contest_id, 
+--     ROUND(COUNT(user_id)::NUMERIC / (SELECT COUNT(*) FROM Users) * 100, 2) AS percentage
+--     FROM Register
+--     GROUP BY contest_id
+--     ORDER BY percentage DESC, contest_id ASC
+    
+WITH TotalUsers AS (
+    SELECT COUNT(*) AS total_users FROM Users
+)
+
+SELECT contest_id, ROUND(COUNT(user_id)::NUMERIC / total_users * 100, 2) AS percentage
+    FROM Register AS R
+    CROSS JOIN TotalUsers AS T
+    GROUP BY contest_id, total_users
     ORDER BY percentage DESC, contest_id ASC
