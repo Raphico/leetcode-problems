dfbeb33cc73caa05c77aa63abe0699a747318a2f
Raphael Awoyinfa <RaphicoGit@gmail.com>
2024-09-05 23:13:27 +0100

    Time: 1128 ms (46.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    contest_id, 
+    ROUND(COUNT(user_id)::NUMERIC / (SELECT COUNT(*) FROM Users) * 100, 2) AS percentage
+    FROM Register
+    GROUP BY contest_id
+    ORDER BY percentage DESC, contest_id ASC
