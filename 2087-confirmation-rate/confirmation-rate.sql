52c91cafb235138ab983cde80a67524731b3a396
Raphael Awoyinfa <RaphicoGit@gmail.com>
2024-09-05 19:31:26 +0100

    Time: 230 ms (97.99%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,10 +1,22 @@
 -- Write your PostgreSQL query statement below
+-- SELECT 
+--     S.user_id, 
+--     CASE 
+--         WHEN COUNT(C.user_id) = 0 THEN 0.00 
+--         ELSE ROUND((COUNT(CASE WHEN C.action = 'confirmed' THEN 1 END) / COUNT(C.user_id)::NUMERIC), 2)
+--         END AS confirmation_rate 
+--     FROM Signups AS S
+--     LEFT JOIN Confirmations AS C
+--         ON S.user_id = C.user_id
+--     GROUP BY S.user_id
+
 SELECT 
     S.user_id, 
-    CASE 
-        WHEN COUNT(C.user_id) = 0 THEN 0.00 
-        ELSE ROUND((COUNT(CASE WHEN C.action = 'confirmed' THEN 1 END) / COUNT(C.user_id)::NUMERIC), 2)
-        END AS confirmation_rate 
+    COALESCE(
+        ROUND((COUNT(CASE WHEN C.action = 'confirmed' THEN 1 END)) /
+              NULLIF(COUNT(C.user_id)::NUMERIC, 0), 2),
+        0.00
+    ) AS confirmation_rate
     FROM Signups AS S
     LEFT JOIN Confirmations AS C
         ON S.user_id = C.user_id
