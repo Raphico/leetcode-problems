60f0b8acac0b01e78436b04f2683e00a26992104
Raphael Awoyinfa <RaphicoGit@gmail.com>
2024-09-05 22:38:30 +0100

    Time: 531 ms (48.98%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    P.project_id,
+    ROUND(AVG(E.experience_years), 2) AS average_years
+    FROM Project AS P
+    LEFT JOIN Employee AS E
+        ON P.employee_id = E.employee_id
+    GROUP BY P.project_id;
