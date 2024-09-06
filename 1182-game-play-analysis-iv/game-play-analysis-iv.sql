39fb722b2bd28fc447ccceafca352ed85d8194d3
Raphael Awoyinfa <RaphicoGit@gmail.com>
2024-09-06 12:55:42 +0100

    Time: 666 ms (31.91%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,15 @@
+-- Write your PostgreSQL query statement below
+WITH FirstLogins AS (
+    SELECT player_id, MIN(event_date) AS first_login_date
+        FROM Activity
+    GROUP BY player_id
+)
+
+SELECT ROUND(
+            COUNT(
+                CASE WHEN A.event_date = F.first_login_date + INTERVAL '1 day' THEN 1 END
+            ) / COUNT(DISTINCT F.player_id)::NUMERIC, 2
+        ) AS fraction
+    FROM Activity AS A
+    JOIN FirstLogins AS F
+        ON F.player_id = A.player_id
