596a0a91fd32318995e2a12e3b31ef4e049fd687
Raphael Awoyinfa <RaphicoGit@gmail.com>
2024-09-11 11:04:56 +0100

    Time: 4610 ms (15.72%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,25 +1,49 @@
 -- Write your PostgreSQL query statement below
-WITH LowSalary AS (
-    SELECT 'Low Salary' AS category, 
-            COUNT(*) AS accounts_count
-        FROM Accounts
-        WHERE income < 20000
-), AverageSalary AS (
-    SELECT 'Average Salary' AS category, 
-            COUNT(*) AS accounts_count
-        FROM Accounts
-        WHERE income >= 20000 
-            AND income <= 50000
-), HighSalary AS (
-    SELECT 'High Salary' AS category, 
-            COUNT(*) AS accounts_count
-        FROM Accounts
-        WHERE income > 50000
+WITH AccountCategories AS (
+    SELECT 'Low Salary' AS category
+    UNION ALL 
+    SELECT 'Average Salary' AS category
+    UNION ALL
+    SELECT 'High Salary' AS category
+), AccountSalaries AS (
+    SELECT 
+        CASE
+            WHEN income < 20000 THEN 'Low Salary'
+            WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
+            WHEN income > 50000 THEN 'High Salary'
+        END AS category,
+        COUNT(*) AS accounts_count
+    FROM Accounts
+    GROUP BY category
 )
 
-SELECT * FROM LowSalary
-UNION ALL
-SELECT * FROM AverageSalary
-UNION ALL
-SELECT * FROM HighSalary
+SELECT A.category,
+       COALESCE(accounts_count, 0) AS accounts_count
+    FROM AccountCategories AS A
+    LEFT JOIN AccountSalaries AS S
+        ON A.category = S.category
+
+-- WITH LowSalary AS (
+--     SELECT 'Low Salary' AS category, 
+--             COUNT(*) AS accounts_count
+--         FROM Accounts
+--         WHERE income < 20000
+-- ), AverageSalary AS (
+--     SELECT 'Average Salary' AS category, 
+--             COUNT(*) AS accounts_count
+--         FROM Accounts
+--         WHERE income >= 20000 
+--             AND income <= 50000
+-- ), HighSalary AS (
+--     SELECT 'High Salary' AS category, 
+--             COUNT(*) AS accounts_count
+--         FROM Accounts
+--         WHERE income > 50000
+-- )
+
+-- SELECT * FROM LowSalary
+-- UNION ALL
+-- SELECT * FROM AverageSalary
+-- UNION ALL
+-- SELECT * FROM HighSalary
 
